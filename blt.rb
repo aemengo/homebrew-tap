813f73e6b1c766f55a9de0d2940f876b34087513
Anthony Emengo <aemengo@pivotal.io>
2018-10-05 18:19:37 -0400

    Bump blt to version v0.4

--- blt.rb
+++ blt.rb
@@ -1,9 +1,9 @@
 class Blt < Formula
   desc "CLI for managing a local BOSH environment."
   homepage "https://github.com/aemengo/blt"
-  version "v0.3"
+  version "v0.4"
   url "https://github.com/aemengo/blt/releases/download/#{version}/blt-#{version}-darwin-amd64"
-  sha256 "dd74341a609dab4be501e193a0c1abc93db23ed4c3404e25a490e9b535fcb5aa"
+  sha256 "f2091206d98240da4fa67e1446a8e350155cc44e4a9b23c4ae20b305846b9753"
 
   depends_on arch: :x86_64
   depends_on 'linuxkit/linuxkit/linuxkit'
